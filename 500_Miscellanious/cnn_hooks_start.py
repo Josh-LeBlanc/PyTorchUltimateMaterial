e4e6c00e001bdc13c0911b025f18d44444b5c24f
Josh LeBlanc <joshleblanc@Joshs-MacBook-Pro-2.local>
2024-08-06 11:52:50 -0400

    ch. 24 model debugging done

--- 500_Miscellanious/cnn_hooks_start.py
+++ 500_Miscellanious/cnn_hooks_start.py
@@ -16,3 +16,31 @@ transformations = transforms.Compose([
 X = transformations(image).unsqueeze(0)
 X.shape
 # %%
+model = resnet18(pretrained=True)
+
+# %%
+class MyHook:
+    def __init__(self):
+        self.layer_out = []
+        self.layer_shape = []
+
+    def __call__(self, module, module_in, module_out):
+        self.layer_out.append(module_out)
+        self.layer_shape.append(module_out.shape)
+# %%
+hook = MyHook()
+for l in model.modules():
+    if isinstance(l, torch.nn.modules.conv.Conv2d):
+        handle = l.register_forward_hook(hook)
+# %%
+y_pred = model(X)
+# %%
+len(hook.layer_out)
+# %%
+layer_num = 0
+layer_imgs = hook.layer_out[layer_num].detach().numpy()
+# %%
+for i in range(4):
+    plt.imshow(layer_imgs[0, i, :, :])
+    plt.show()
+# %%
